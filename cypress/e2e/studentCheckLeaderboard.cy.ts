18d1260b9778a1fb6b53023bf0369b571b34686a
Alexander Larsson <alexander.larsson@edu.newton.se>
2025-09-23 16:58:13 +0200

    tog bort felbeskrivande kommentar

--- cypress/e2e/studentCheckLeaderboard.cy.ts
+++ cypress/e2e/studentCheckLeaderboard.cy.ts
@@ -4,11 +4,10 @@ describe('Check leaderboard', () => {
         cy.visit('/studentDashboard');
     });
 
-    it('should be able to check your leaderboard standing', () => {
+    it('should be able to check leaderboard standing', () => {
         cy.contains('Prestationer').should('be.visible');
         cy.contains('button', 'Prestationer').click();
         cy.url().should('include', '/leaderboard');
-        //kollar rubriker eftersom alla andra värden kan ändras
         cy.contains('MINA RESULTAT').should('be.visible');
         cy.contains('ÄMNE').should('be.visible');
         cy.contains('TOPPLISTA').should('be.visible');
